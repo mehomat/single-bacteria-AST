b36ef779094a8b80a9cc663f6eb45b4c2166cd62
Lisa Johansson <lijo7489@pelle1.uppmax.uu.se>
2026-02-12 11:20:53 +0100

    Updated outputs, requirements for anchor, etc

--- getValidTraps.m
+++ getValidTraps.m
@@ -1,5 +1,5 @@
 function S = getValidTraps(expInfoObj, pos, switchFrame, dt, varargin)
-%
+
 % Identify valid traps and return mother cell IDs per trap
 % 
 % Input:
@@ -12,7 +12,7 @@ function S = getValidTraps(expInfoObj, pos, switchFrame, dt, varargin)
 % - 'Traps': array of trap indices, default is all growth channels
 % - 'YThresh': scalar or [], default is []
 % - 'MinCellGR': scalar, default is 0.002
-% - 'PreMarginMinutes': scalar minutes, default is 15
+% - 'PreMarginMinutes': scalar minutes, default is 0
 % - 'TrackFrameRange': [start end] frames, default is [switchFrame-10/dt, switchFrame+5/dt])
 % - 'RequireDividing': logical, default is false
 %
@@ -25,14 +25,16 @@ function S = getValidTraps(expInfoObj, pos, switchFrame, dt, varargin)
 % mother cells found in that trap and sorted by birth frame
 % S(ti).isValid is a logical flag and is only true if the trap passed the
 % checks and if motherIds is not empty
+% S(ti).growthDir is the growth direction of the cells based on the sign of
+% meanshifty
 
 p = inputParser;
 p.addParameter('Traps', [], @(x) isnumeric(x));
 p.addParameter('YThresh', []);
 p.addParameter('MinCellGR', 0.002, @(x) isnumeric(x) && isscalar(x));
-p.addParameter('PreMarginMinutes', 15, @(x) isnumeric(x) && isscalar(x));
+p.addParameter('PreMarginMinutes', 0, @(x) isnumeric(x) && isscalar(x));
 p.addParameter('TrackFrameRange', [switchFrame-10/dt, switchFrame+5/dt], @(x) isnumeric(x) && numel(x)==2);
-p.addParameter('RequireDividing', false, @(x) islogical(x) || isnumeric(x));
+p.addParameter('RequireDividingBeforeSwitch', false, @(x) islogical(x) || isnumeric(x));
 p.parse(varargin{:});
 
 trapRange = p.Results.Traps;
@@ -40,7 +42,7 @@ yThresh = p.Results.YThresh;
 minCellGR = p.Results.MinCellGR;
 preMarginMinutes = p.Results.PreMarginMinutes;
 trackFrameRange = p.Results.TrackFrameRange;
-requireDividing = logical(p.Results.RequireDividing);
+requireDividingBeforeSwitch = logical(p.Results.RequireDividingBeforeSwitch);
 
 param = expInfoObj.getParameters();
 nGrowthChannels = param.nGrowthChannels - length(param.emptyChannel);
@@ -53,6 +55,11 @@ mCells = expInfoObj.getMCells(pos);
 birthFrames = [mCells.birthFrame];
 lastFrames = [mCells.lastFrame];
 cellTraps = [mCells.growthChannel];
+lifeTimes = [mCells.lifeTime];
+isBadCellVals = [mCells.isBadCell];
+
+% isBadCell filter
+okBadCell = (isBadCellVals == 0 | isBadCellVals == 4);
 
 % Per cell properties
 cellYcoord = nan(size(cellTraps));
@@ -78,6 +85,9 @@ end
 dy = quantile(cellLengths(birthFrames < switchFrame), 0.1) / 2;
 meanshifty = mean(shifty(lastFrames < switchFrame), 'omitnan');
 
+% Save growth direction as an output
+growthDir = sign(meanshifty);
+
 if meanshifty < 0
     cellYcoord = cellYcoord + cellLengths;
 end
@@ -100,30 +110,39 @@ cutoffFrame = max(switchFrame - preMarginFrames, 1);
 
 % Output
 nTraps = numel(trapRange);
-S(nTraps) = struct('pos', [], 'trap', [], 'trapID', [], 'motherIds', [],  'isValid', [] );
+S(nTraps) = struct('pos', [], 'trap', [], 'trapID', [], 'motherIds', [],  'isValid', [], 'growthDir', []);
 
 trapIDs = (pos-1) * nGrowthChannels + trapRange;
 for ti = 1:numel(trapRange)
     trap = trapRange(ti);
 
     % Anchor set, goodGrowth anchors the mother band
-    indAnchor = (cellTraps == trap & goodGrowth);
-    if requireDividing
-        indAnchor = indAnchor & isDividing;
-    end
+    indAnchor = (cellTraps == trap & goodGrowth & okBadCell);
 
     if any(indAnchor)
         if meanshifty > 0
             yCutOff = min(cellYcoord(indAnchor), [], 'omitnan') + dy;
             trapMotherCellIds = find(cellTraps == trap & cellYcoord < yCutOff);
+            trapMotherCellIds = trapMotherCellIds(okBadCell(trapMotherCellIds));
         else
             yCutOff = max(cellYcoord(indAnchor), [], 'omitnan') - dy;
             trapMotherCellIds = find(cellTraps == trap & cellYcoord > yCutOff);
+            trapMotherCellIds = trapMotherCellIds(okBadCell(trapMotherCellIds));
         end
 
         % Remove early born mothers that are not growing
         removeInd = birthFrames(trapMotherCellIds) < cutoffFrame & ~goodGrowth(trapMotherCellIds);
         trapMotherCellIds(removeInd) = [];
+
+        % If mother cell has good enough GR before switch, but it has not
+        % divided yet and one needs the last cell area before
+        % division for baseline purposes
+        if requireDividingBeforeSwitch
+
+            trapMotherCellIds = trapMotherCellIds( ...
+                isDividing(trapMotherCellIds) & (lastFrames(trapMotherCellIds) < switchFrame) );
+        end
+
     else
         trapMotherCellIds = [];
     end
@@ -150,7 +169,8 @@ for ti = 1:numel(trapRange)
     S(ti).trapID = trapIDs(ti);
     S(ti).motherIds = trapMotherCellIds(:); % column vector
     S(ti).isValid = isValid && ~isempty(trapMotherCellIds);
-    
+    S(ti).growthDir = growthDir; 
+   
 end
 
 end
